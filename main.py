eba03bd724f570b6d5a0b65a7780fac591864893
hongbin-xu <36011407+hongbin-xu@users.noreply.github.com>
2023-09-21 16:20:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import streamlit_authenticator as stauth
+#import streamlit_authenticator as stauth
 
 st.title('IAC-Rutting Verification')
 
