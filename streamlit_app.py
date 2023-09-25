dd684a8db70b186c5831421929541ae24a72f06c
hongbin-xu <36011407+hongbin-xu@users.noreply.github.com>
2023-09-25 14:51:48 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,7 @@ def surfPlot(dataArray, tranStep, lonStep):
     fig = px.imshow(dataArray, origin = "lower", labels = {"x": "Transverse (mm)", "y": "Longitudinal (mm)", "color": "DEPTH (mm)"},
                     x =np.arange(1536)*tranStep,
                     y = np.arange(900)*lonStep,
-                    width = 500)
+                    width = 1000)
     fig.update_layout(legend = {"orientation":"h", "xanchor": "left", "x":1.01, "yanchor": "top", "y":1})
     st.plotly_chart(fig)
 
