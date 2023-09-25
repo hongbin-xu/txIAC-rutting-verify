ac152c02ad57294647eabc7b8554763225045558
hongbin-xu <36011407+hongbin-xu@users.noreply.github.com>
2023-09-25 10:21:22 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,13 +33,10 @@ def check_password():
         return True
 
 if check_password():
-    DATE_COLUMN = 'date/time'
-    DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-                'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
-    st.set_page_config(
-        page_title='IAC-Rutting Verification', 
-        )
-    
+    # Page title
+    st.set_page_config(page_title='IAC-Rutting Verification')
+
+    # sidebar
     with st.sidebar:
         st.title("TxDOT Inter-Agency Contract")
         st.subheader("Rutting Measurement Verification")
@@ -47,22 +44,9 @@ if check_password():
         st.text("Presented by Hongbin Xu and Jorge Prozzi")
         st.text("The University of Texas at Austin")
     
+    # Mysql connection
     conn = st.experimental_connection('mysql', type='sql')
-    
-    @st.cache_data
-    def load_data(nrows):
-        data = pd.read_csv(DATA_URL, nrows=nrows)
-        lowercase = lambda x: str(x).lower()
-        data.rename(lowercase, axis='columns', inplace=True)
-        data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-        return data
-    
-    data_loading = st.text('Loading data...')
-    data1 = load_data(10000)
-    data_loading.text("Done!")
-    
     col1, col2 = st.columns([3,4])
-    
     with col1:
         with st.container():
             st.subheader("Suface")
@@ -71,8 +55,7 @@ if check_password():
                 segID = st.number_input("Segment ID", min_value=1, max_value=100, step= 1)
             with col12:
                 scanID = st.number_input("Scan ID", min_value=0, max_value=899, step = 1)
-            st.write(segID)
-            #data = pd.read_csv()
+            #data = pd.
     
             with st.container():
                 # Some number in the range 0-23
@@ -82,10 +65,7 @@ if check_password():
     
     with col2:
         with st.container():
-            st.subheader("Transverse Profile")
-    
-    
-    
+            st.subheader("Transverse Profile")   
             hist_values = np.histogram(data1[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
             st.bar_chart(hist_values)
             if st.checkbox('Show raw transverse profile data'):
