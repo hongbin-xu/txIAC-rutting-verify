72374f294e5635c6061b2e57edd8b22ced32eb6b
hongbin-xu <36011407+hongbin-xu@users.noreply.github.com>
2023-09-26 14:28:12 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -108,9 +108,9 @@ if check_password():
                 with col12:
                     st.write(data.head())
                     idSelect = st.number_input("Line", min_value=idmin, max_value=idmax, step = 1)
-                    st.write(data.loc[data["id"]==idSelect, ["segID"]].reset_index(drop = True))
-                    segID = data.loc[data["id"]==idSelect, ["segID"]].reset_index(drop = True)[0]
-                    scanID = data.loc[data["id"]==idSelect, ["scanID"]].reset_index(drop = True)[0]
+                    st.write(data.loc[data["id"]==(idSelect+1), ["segID"]].reset_index(drop = True))
+                    segID = data.loc[data["id"]==(idSelect+1), ["segID"]].reset_index(drop = True)[0]
+                    scanID = data.loc[data["id"]==(idSelect+1), ["scanID"]].reset_index(drop = True)[0]
             st.write("Route: "+ str(data["ROUTE_NAME"][0])+ ", DFO: "+str(data["DFO"].min())+ "~"+ str(data["DFO"].max()))
             # plot surface
             with st.container():
