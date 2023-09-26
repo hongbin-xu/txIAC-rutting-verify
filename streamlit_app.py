b3ab41cc313743df2e39c10f6e2e4dec767726bb
hongbin-xu <36011407+hongbin-xu@users.noreply.github.com>
2023-09-26 14:25:35 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -106,6 +106,7 @@ if check_password():
                     # Load data
                     data, tranStep, lonStep, dataArray = dataLoad(_conn=conn, idmin= idmin+1, idmax=idmax+1, mode ="2")
                 with col12:
+                    st.write(data.head())
                     idSelect = st.number_input("Line", min_value=idmin, max_value=idmax, step = 1)
                     st.write(data.loc[data["id"]==idSelect, ["segID"]].reset_index(drop = True))
                     segID = data.loc[data["id"]==idSelect, ["segID"]].reset_index(drop = True)[0]
